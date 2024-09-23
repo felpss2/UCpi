314834d1f45ee6758e211dfdda0a1cd05e28fdf8
Felype <felypeperesecher@gmail.com>
2024-09-23 11:52:21 -0300

    commitando

--- loja/server.js
+++ loja/server.js
@@ -27,16 +27,16 @@ connection.connect(error => {
 
 // Endpoint para adicionar um produto (POST)
 
-app.post('/produtos,',(req, res) => {
-    const {nome, descricao, preco} = req.body;
+app.post('/produtos,', (req, res) => {
+    const { nome, descricao, preco } = req.body;
     const sql = 'INSERT INTO produtos (nome, descricao, preco) VALUES (?,?,?)';
 
     connection.query(sql, [nome, descricao, preco], (error, results) => {
-       if (error) {
-        res.status(500).send('Erro ao adicionar produto.');
-        return;
-       }
-       res.status(201).send('Produto adicionado com sucesso.');
+        if (error) {
+            res.status(500).send('Erro ao adicionar produto.');
+            return;
+        }
+        res.status(201).send('Produto adicionado com sucesso.');
     });
 
 });
@@ -44,6 +44,58 @@ app.post('/produtos,',(req, res) => {
 // Endpoint para obter todos os produtos (GET)
 
 app.get('/produtos', (req, res) => {
+    connection.query('SELECT * FROM produtos', (error, results) => {
+        if (error) {
+            res.status(500).send('Erro ao obter usuário.');
+            return;
+        }
+        res.json(results[0]);
+    });
+});
 
-    
+// Endpoint para obter um produto por ID (GET)
+
+app.get('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query('SELECT FROM produtos WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            res.status(500).send('Error ao obter usuário.');
+            return;
+        }
+        res.json(results[0]);
+    });
+});
+
+// Endpoint para atualizar um produto (PUT)
+
+app.put('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, descricao, preco } = req.body;
+    const sql = 'UPDATE produtos SET nome = ?, descricao = ?, preco = ? WHERE id = ?';
+    connection.query(sql, [nome, descricao, preco, id], (error, results) => {
+        if (error) {
+            res.status(500).send('Error ao atualizar produto.');
+            return;
+        }
+        res.send('Produto atualizado com sucesso.');
+    });
+});
+
+// Endpoint para deletar um produto (DELETE)
+app.delete('/produtos/:id', (req, res) => {
+    const { id } =  req.params;
+    connection.query('DELETE FROM produtos WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            res.status(500).send('Erro ao deletar produto.');
+            return;
+        }
+        res.send('Produto deletado com sucesso.');
+    });
+});
+
+//Iniciar o servidor
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 })
