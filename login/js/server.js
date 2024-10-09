c14f765bab66374256e234b89e441cf96e3a8717
Felype <felypeperesecher@gmail.com>
2024-10-08 21:48:23 -0300

    commitei

--- login/js/server.js
+++ login/js/server.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+
+
+const SECRET_KEY = 'faz_o_l'; // Troque para um segredo seguro
+
 
 app.use(cors(/*{
     origin: 'http://127.0.0.1:5500'
@@ -16,7 +21,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'login'
 });
 
@@ -46,19 +51,24 @@ app.post('/login', (req, res) => {
 app.post('/register', async (req, res) => {
     const { email, password } = req.body;
 
-    try {
-        // Gera o hash da senha com um salt de 10 rounds
-        const  hashedPassword = await bcrypt.hash(password, 10);
+
+    // Gera o hash da senha com um salt de 10 rounds
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            return res.status(400).send('Usuário já existe');
+        }
 
         // Insere o email e a senha criptografada no banco de dados
         db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword], (err, result) => {
             if (err) throw err;
             res.sendStatus(201); // Usuário registrado com sucesso
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erro ao registrar usuário');
-    }
+        } );
+
+    });
+
 });
 
 
